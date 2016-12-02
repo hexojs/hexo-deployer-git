e33540490ffa53b812a278f99a8967f4d31b0193
Abner Chou <zhcjtht@hotmail.com>
2016-12-02 17:26:13 -0500

    Update deployer.js

--- test/deployer.js
+++ test/deployer.js
@@ -132,4 +132,23 @@ describe('deployer', function() {
       return validate();
     });
   });
+  
+  it('hidden file', function() {
+    return fs.writeFile(pathFn.join(publicDir, '.hid'), 'hidden')
+    .then(function() {
+      return deployer({
+        repo: fakeRemote,
+        ignore_hidden: false,
+        silent: true
+      });
+    }).then(function() {
+      return validate();
+    }).then(function() {
+      var hidTxtFile = pathFn.join(publicDir, '.hid');
+
+      return fs.readFile(hidTxtFile);
+    }).then(function(content) {
+      content.should.eql('hidden');
+    });
+  });
 });
